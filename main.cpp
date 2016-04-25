74afecb3c5795226589aab57a95555a4fe3fef71
Courtney <courtney.snyder23@yahoo.com>
2016-04-25 16:42:29 -0700

    Added files via upload
    testmain uses what Michael posted 4 days ago as the format for the level, renamed as a function called level. main calls the level function and invokes the constructor for Universe.

--- main.cpp
+++ main.cpp
@@ -1,40 +1,20 @@
 /*
 Programmers: Courtney Snyder, Greg Wallace, Michael LaFountain
-Last Updated: 4/18/16
+Last Updated: 4/25/16
 Description: A game of gravity golf.
 */
 #include "Body.h"
 
-int main() 
+int main(void)
 {
-	sf::RenderWindow window(sf::VideoMode(640, 480), "Gravity Golf");
-	double fps = 60;
-	sf::Clock clock;
-	sf::Time currentTime;
-	nonstaticBody ball;
-	float x = 0, y = 0;
-	int level = 0; //Indicates where the player is in the game (level 0 is the tutorial)
-	while (window.isOpen())
-	{
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed) //If the user clicks the X
-				window.close(); //Close the window
-			sf::CircleShape goal;
-			Universe map;
-			goal.setRadius(20);
-			goal.setFillColor(sf::Color::Green);
-			goal.setPosition(600, 400);
-			switch (level)
-			{
-			case 0: //Tutorial
-				level = tutorial(level, window, map, goal);
-			case 1: //Level 1
-				break;
-			default:
-				break;
-			}		}
-	}
+	double mercury = 330, venus = 0, earth = 0, mars = 0, jupiter = 0, saturn = 0, uranus = 0, neptune = 0, pluto = 0;
+	double initMass[100];
+	double initXPos[100];
+	double initYPos[100];
+	double initXVel[100];
+	double initYVel[100];
+	Universe tutorial, level1(1, initMass, initXPos, initYPos, initXVel, initYVel);
+	level(tutorial, 200, 200);
+	level(level1, 400, 400);
 	return 0;
 }
